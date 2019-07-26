d4e8e5591eb3d4a518dffaf45fe034459545db2f
liyongjie <liyongjie@tvflnet.com>
2019-07-26 21:22:07 +0800

    实现永久图片视频图文素材的上传和数量查询

--- wechat-lib/index.js
+++ wechat-lib/index.js
@@ -5,8 +5,20 @@ const base = 'https://api.weixin.qq.com/cgi-bin/'
 
 const api = {
     accessToken: base + 'token?grant_type=client_credential',
+    // 临时上传
     temporary: {
         upload: base + 'media/upload?'
+    },
+    // 永久上传
+    permanent: {
+        upload: base + 'material/add_material?',
+        uploadNews: base + 'material/add_news?',
+        uploadNewsPic: base + 'material/upload_img?',
+        fetch: base + 'material/get_material?',
+        batch: base + 'material/batchget_material?',
+        count: base + 'material/get_materialcount?',
+        del: base + 'material/del_material?',
+        update: base + 'material/update_news?',
     }
 }
 
@@ -51,7 +63,7 @@ module.exports = class Wechat {
     }
     // 向微信端请求token
     async updateAccessToken() {
-        const url = `${api.accessToken}&appid=${this.appID}&secret=${this.appSecret}`
+        let url = `${api.accessToken}&appid=${this.appID}&secret=${this.appSecret}`
 
         const data = await this.request({ url })
         const now = new Date().getTime()
@@ -81,18 +93,91 @@ module.exports = class Wechat {
         let form = {}
         let url = api.temporary.upload
         if (permanent) {
-
+            url = api.permanent.upload
+            //??
+            form = Object.assign(form, permanent)
         }
-        form.media = fs.createReadStream(material)
-        let uploadUrl = `${url}access_token=${token}&type=${type}`
+        if ('pic' === type) {
+            url = api.permanent.uploadNewsPic
+        }
+        if ('news' === type) {
+            url = api.permanent.uploadNews
+            form.material
+        } else {
+            form.media = fs.createReadStream(material)
+        }
+
+        let uploadUrl = `${url}access_token=${token}`
+        if (!permanent) {
+            uploadUrl += `&type=${type}`
+        } else {
+            if (type !== 'news') {
+                form.access_token = token;
+            }
+        }
+
         const options = {
             method: 'POST',
             url: uploadUrl,
             json: true,
-            formData: form
         }
+
+        if ('news' === type) {
+            options.body = form
+        } else {
+            options.formData = form
+        }
+
         return options
     }
+
+    deleteMaterial(token, mediaId) {
+        const form = {
+            media_id = mediaId
+        }
+        let url = `${api.permanent.del}access_token=${token}&media_id=${mediaId}`
+
+        return {
+            method: 'POST',
+            url,
+            body: form
+        }
+    }
+
+    updateMaterial(token, mediaId, news) {
+        let form = {
+            media_id: mediaId
+        }
+        form = Object.assign(form, news)
+        let url = `${api.permanent.update}access_token=${token}&media_id=${mediaId}`
+        return {
+            method: 'POST',
+            url,
+            body: form
+        }
+    }
+
+    countMaterial(token) {
+        let url = `${api.permanent.count}access_token=${token}`;
+        return {
+            url
+        };
+    }
+
+    batchMaterial(token, options) {
+        options.type = options.type || 'image';
+        options.offset = options.offset || 0;
+        options.count = options.count || 10;
+
+        let url = `${api.permanent.batch}access_token=${token}`;
+
+        return {
+            method: 'POST',
+            url,
+            body: options,
+        };
+    }
+
     async handle(operation, ...args) {
         const token = await this.fetchAccessToken()
         const options = this[operation](token.access_token, ...args)
--- wechat/reply.js
+++ wechat/reply.js
@@ -31,9 +31,91 @@ exports.reply = async (ctx, next) => {
                 description: '吃个鸡？',
                 mediaId: data.media_id
             }
+        } else if (content === '6') {
+            let data = await client.handle('uploadMaterial', 'video', path.resolve(__dirname, '../6.mp4'), {
+                type: 'video',
+                description: '{"title": "吃个鸡？", "introduction": "吃个鸡？"}'
+            })
+            reply = {
+                type: 'video',
+                title: '回复的视频标题',
+                description: '吃个鸡？',
+                mediaId: data.media_id
+            }
         }
-        else if (content === '4') {
-            reply = '没了'
+        else if ("7" === content) {
+            let data = await client.handle('uploadMaterial', 'image', resolve(__dirname, '../2.jpg'), {
+                type: 'image',
+            });
+            reply = {
+                type: 'image',
+                mediaId: data.media_id,
+            };
+        } else if ("8" === content) {
+            let data = await client.handle('uploadMaterial', 'image', resolve(__dirname, '../2.jpg'), {
+                type: 'image',
+            });
+            let data2 = await client.handle('uploadMaterial', 'pic', resolve(__dirname, '../2.jpg'), {
+                type: 'image',
+            });
+            let media = {
+                articles: [
+                    {
+                        "title": "这是服务端上传的图文 1",
+                        "thumb_media_id": data.media_id,
+                        "author": "Angus",
+                        "digest": "没有摘要",
+                        "show_cover_pic": 1,
+                        "content": "点击去往我的博客",
+                        "content_source_url": "http://www.feihu1996.cn",
+                    },
+                    {
+                        "title": "这是服务端上传的图文 2",
+                        "thumb_media_id": data2.media_id,
+                        "author": "Angus",
+                        "digest": "没有摘要",
+                        "show_cover_pic": 1,
+                        "content": "点击去往GitHub",
+                        "content_source_url": "https://github.com/",
+                    },
+                ],
+            };
+            let uploadData = await client.handle('uploadMaterial', "news", media, {});
+            reply = {
+                type: 'image',
+                mediaId: data.media_id,
+            };
+        } else if ("9" === content) {
+            let counts = await client.handle('countMaterial');
+            let res = await Promise.all([
+                client.handle('batchMaterial', {
+                    type: 'image',
+                    offset: 0,
+                    count: 10
+                }),
+                client.handle('batchMaterial', {
+                    type: 'video',
+                    offset: 0,
+                    count: 10
+                }),
+                client.handle('batchMaterial', {
+                    type: 'voice',
+                    offset: 0,
+                    count: 10
+                }),
+                client.handle('batchMaterial', {
+                    type: 'news',
+                    offset: 0,
+                    count: 10
+                }),
+            ]);
+            console.log(res);
+            reply = `
+            image: ${res[0].total_count}
+            video: ${res[1].total_count}
+            voice: ${res[2].total_count}
+            news: ${res[3].total_count}
+            `
         }
 
         ctx.body = reply
